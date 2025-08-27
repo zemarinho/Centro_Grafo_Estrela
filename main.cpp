36ee9e5d2c9f6848217481e27923db3e2e6fc4f4
zemarinho <zemiguelmarinho05@hotmail.com>
2025-08-27 16:38:46 +0100

    Funcional mas sem contenções
    Signed-off-by: zemarinho <zemiguelmarinho05@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+//RETORNA O NÓ CENTRAL DE UM GRAFO EM ESTRELA
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n, cent=0;
+        set<int> s;
+        for (int i=0; i<edges.size(); i++){
+            if (!s.insert(edges[i][0]).second){
+                return edges[i][0];
+            }
+            if (!s.insert(edges[i][1]).second){
+                return edges[i][1];
+            }
+        }
+        return -1;
+    }
+};
 
 int main(){
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
